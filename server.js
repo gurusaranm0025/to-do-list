2a7568003fbeab4be9442c33cedc79e216ede88d
Guru Saran M <gurusaranm0025@gmail.com>
2023-07-30 17:53:29 +0530

    finished the work for seperating the personal list from work list made some performance improvements

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 var todolist = []
+var workTodoList = []
 
 app.get("/", (req,res) => {
-    res.render("home.ejs", { todos: todolist });
+    res.render("home.ejs", { todos: todolist, toPersonal: true });
+});
+
+app.get("/work", (req, res) => {
+    res.render("home.ejs", {todos: workTodoList, toPersonal: false });
 });
 
 app.post("/add", (req,res) => {
@@ -17,6 +22,12 @@ app.post("/add", (req,res) => {
     todolist.push(req.body.newTodo);
     res.redirect("/");
 });
+
+app.post("/work", (req, res) => {
+    console.log("post recieved to work list", req.body.newTodo);
+    workTodoList.push(req.body.newTodo);
+    res.redirect("/work");
+})
 
 app.listen(port, () => {
     console.log(`Server is successfully running on port ${port}`);
