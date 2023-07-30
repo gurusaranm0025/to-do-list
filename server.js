2e52e00ae9c4e6c0b27ca84748a4b6a00f6e5a60
Guru Saran M <gurusaranm0025@gmail.com>
2023-07-30 17:42:13 +0530

    finished basic backend works includes getting input and displaying it.

--- server.js
+++ server.js
@@ -6,8 +6,16 @@ const port = 6002;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+var todolist = []
+
 app.get("/", (req,res) => {
-    res.render("home.ejs");
+    res.render("home.ejs", { todos: todolist });
+});
+
+app.post("/add", (req,res) => {
+    console.log("post recieved", req.body.newTodo);
+    todolist.push(req.body.newTodo);
+    res.redirect("/");
 });
 
 app.listen(port, () => {
