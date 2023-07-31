4d1d9803a75006b2bc722bbf49fe74ad8ddfcbe7
Guru Saran M <gurusaranm0025@gmail.com>
2023-07-31 23:09:03 +0530

    added css code to strike the finished todos, modified the server program to add default code automatically and improved the performance and code readabilty in home.ejs

--- server.js
+++ server.js
@@ -1,64 +1,83 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 const mongoose = require("mongoose");
 
-mongoose.connection("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+});
 
 const itemsSchema = new mongoose.Schema({
-    name: String
+  name: String,
 });
 
 const Item = new mongoose.model("Item", itemsSchema);
 
-const item1 = new Item ({
-    name: "Welcome to the todolist!"
+const item1 = new Item({
+  name: "Welcome to the todolist!",
 });
 
-const item2 = new Item ({
-    name:"Hit the + buton add a to-do to the list"
+const item2 = new Item({
+  name: "Hit the + buton add a to-do to the list",
 });
 
-const item3 = new Item ({
-    name: "<---Hit this to delete the item."
+const item3 = new Item({
+  name: "<---Hit this to delete the item.",
 });
 
-const defaultItems = [item1,item2,item3];
+const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems).then(function() {
-    console.log("Default data is successfully inserted.");
-}).catch(function(err) {
-    console.log(err);
-});
-
+// Code to add deafult items to mongoDB
 
 const app = express();
 const port = 6002;
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var todolist = []
-var workTodoList = []
+var todolist = [];
+var workTodoList = [];
 
-app.get("/", (req,res) => {
-    res.render("home.ejs", { todos: todolist, toPersonal: true });
+app.get("/", (req, res) => {
+  Item.find({})
+    .then(function (items) {
+      if (items.length === 0) {
+        Item.insertMany(defaultItems)
+          .then(function () {
+            console.log("Default data is successfully inserted.");
+            res.redirect("/");
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+      } else {
+        res.render("home.ejs", {
+          todos: items,
+          toPersonal: true,
+        });
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
 app.get("/work", (req, res) => {
-    res.render("home.ejs", {todos: workTodoList, toPersonal: false });
+  res.render("home.ejs", { todos: workTodoList, toPersonal: false });
 });
 
-app.post("/add", (req,res) => {
-    console.log("post recieved", req.body.newTodo);
-    todolist.push(req.body.newTodo);
-    res.redirect("/");
+app.post("/add", (req, res) => {
+  console.log("post recieved", req.body.newTodo);
+  todolist.push(req.body.newTodo);
+  res.redirect("/");
 });
 
 app.post("/work", (req, res) => {
-    console.log("post recieved to work list", req.body.newTodo);
-    workTodoList.push(req.body.newTodo);
-    res.redirect("/work");
-})
+  console.log("post recieved to work list", req.body.newTodo);
+  workTodoList.push(req.body.newTodo);
+  res.redirect("/work");
+});
 
 app.listen(port, () => {
-    console.log(`Server is successfully running on port ${port}`);
+  console.log(`Server is successfully running on port ${port}`);
 });
